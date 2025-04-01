de3766764e10d30ed21a4b3723000d34f61c9ffc
Arshak Zaqaryan <arshakzakaryan2004@gmail.com>
2025-04-01 16:36:39 +0400

    fixed issue

--- C/homework-5/task4.c
+++ C/homework-5/task4.c
@@ -10,11 +10,9 @@ int main() {
                 scanf("%d", &arr[i]);
         }
 
-        int min = arr[0];
         int index = 0;
         for (int i = 1; i < n; ++i) {
-                if (arr[i] < min) {
-                        min = arr[i];
+                if (arr[i] < arr[index]) {
                         index = i;
                 }
         }
