b27c6a4fcdd25b1dffb6b2f0192060a7cf5cd7e1
Arshak Zaqaryan <arshakzakaryan2004@gmail.com>
2025-04-02 00:54:17 +0400

    fixed issues

--- C/homework-6/task7.c
+++ C/homework-6/task7.c
@@ -12,7 +12,7 @@ int main(){
 
 	_Bool check = 1;		// bool for checking if the massive is sorted in descending order
 	for(int i = 1; i < n; ++i){
-		if(arr[i] < arr[i+1]){
+		if(arr[i] > arr[i-1]){
 			printf("No\n");
 			check = 0;
 			break;
--- C/homework-6/task8.c
+++ C/homework-6/task8.c
@@ -2,16 +2,23 @@
 
 int main(){
 
-	int arr[] = {1, 1, 2, 2, 3, 3, 4, 5, 5, 6, 6, 7, 7};
+	int arr[] = {1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8};
 	const int n = sizeof(arr)/sizeof(arr[0]);
 
 	int unique = 0;
-	for(int i = 0; i < n; i += 2){
+	_Bool last = 1;
+	for(int i = 0; i < n - 2; i += 2){
 		if(arr[i] != arr[i + 1]){
-			unique = arr[i];
+			unique = arr[i + 1];
 			printf("The unique element is %d\n", unique);
+			last = 0;
 			break;	
 		}
+	}
+
+	if(last){
+		unique = arr[n - 1];
+		printf("The unique element is %d\n", unique);
 	}
 
 	return 0;
